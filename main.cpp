85f8017601bb8b0ed5e79afadd262696d4f9d8a9
Harry Lawal <103933304+Harrylawal74@users.noreply.github.com>
2025-03-03 13:46:51 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include "HX711.h"
 #include "<math.h>"
 
+// set the LCD address to 0x64 for a 20 chars and 4 line display
+LiquidCrystal_I2C lcd(0x64,20,4);
+
+
+// add button pin for I/O button 
+const int onButtonPin = 2;
+
+// add button pin for pH sensor calibrator button 
+const int phButtonPin = 3;
+
+// add button pin for tare button 
+const int tareButtonPin 4;
+
+//add button pin for weight percentage change button
+const int weightPercentButtonPin = 5;
+
+
 
 
 void setup() {
@@ -10,6 +27,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if 
 
 }
