0f7194ef22cd82c86b2b51b7592116d01ed84b2b
Harry Lawal <103933304+Harrylawal74@users.noreply.github.com>
2025-03-16 20:44:55 +0000

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+#include "HX711.h" // Library for analog HX711
+
+
+#define POWER_BUTTON_PIN *********
+bool systemPower = FALSE;
+
+void setup() {
+
+  pinMode(POWER_BUTTON_PIN, INPUT_PULLUP); //power button
+
+}
+
+void loop() {
+ 
+}
+
+
+void onOff(){
+  if (digitalRead(POWER_BUTTON_PIN) == LOW) {
+
+    delay(2000); // delay I/O button press for 5 seconds
+    if (digitalRead(POWER_BUTTON_PIN) == LOW) {
+
+      if (systenPower == TRUE) {
+        systemPower = FALSE;
+      } 
+      else {
+        systemPower = TRUE;
+      }
+    }
+  }
+}
